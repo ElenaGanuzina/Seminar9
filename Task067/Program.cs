e2c62a11d6c0b90bd0a9ecf53ac9655c43d198fd
ElenaG <freshelf@yandex.ru>
2022-09-02 09:06:03 +0500

    add Task067

---
+++ Task067/Program.cs
@@ -0,0 +1,13 @@
+﻿// вход - число, выход - сумма цифр числа.
+
+int SumNumbers(int number)
+{
+    if(number == 0) 
+        return 0;
+    
+    
+    return (number % 10) + SumNumbers(number / 10);
+    
+}
+
+Console.WriteLine(SumNumbers(753));
