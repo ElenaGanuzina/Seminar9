32b87d6694d0130ae5e2a332c093c72570bd732b
ElenaG <freshelf@yandex.ru>
2022-09-02 09:06:29 +0500

    add Task069

---
+++ Task069/Program.cs
@@ -0,0 +1,15 @@
+﻿// A и B, A в степени B
+
+
+int Exponentiation(int a, int b)
+{
+    if(b == 0) 
+        return 1;
+    
+    return a * Exponentiation(a, b - 1);
+}
+
+Console.WriteLine(Exponentiation(3, 5));
+Console.WriteLine(Exponentiation(2, 4));
+Console.WriteLine(Exponentiation(10, 3));
+Console.WriteLine(Exponentiation(5, 4));
